ea8b93ea8cb42c262657239a644f21d776946837
prerak srivastava <prerak.s@research.iiit.ac.in>
2022-01-30 01:36:04 +0530

    updated boilerplate

---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/prerak/IIIT/cg/ass1/2DGame/src/game.cpp" "CMakeFiles/game.dir/src/game.cpp.o" "gcc" "CMakeFiles/game.dir/src/game.cpp.o.d"
+  "/home/prerak/IIIT/cg/ass1/2DGame/src/main.cpp" "CMakeFiles/game.dir/src/main.cpp.o" "gcc" "CMakeFiles/game.dir/src/main.cpp.o.d"
+  "/home/prerak/IIIT/cg/ass1/2DGame/src/resource_manager.cpp" "CMakeFiles/game.dir/src/resource_manager.cpp.o" "gcc" "CMakeFiles/game.dir/src/resource_manager.cpp.o.d"
+  "/home/prerak/IIIT/cg/ass1/2DGame/src/shader.cpp" "CMakeFiles/game.dir/src/shader.cpp.o" "gcc" "CMakeFiles/game.dir/src/shader.cpp.o.d"
+  "/home/prerak/IIIT/cg/ass1/2DGame/src/sprite_renderer.cpp" "CMakeFiles/game.dir/src/sprite_renderer.cpp.o" "gcc" "CMakeFiles/game.dir/src/sprite_renderer.cpp.o.d"
+  "/home/prerak/IIIT/cg/ass1/2DGame/src/stb_image.cpp" "CMakeFiles/game.dir/src/stb_image.cpp.o" "gcc" "CMakeFiles/game.dir/src/stb_image.cpp.o.d"
+  "/home/prerak/IIIT/cg/ass1/2DGame/src/texture.cpp" "CMakeFiles/game.dir/src/texture.cpp.o" "gcc" "CMakeFiles/game.dir/src/texture.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/prerak/IIIT/cg/ass1/2DGame/build/libraries/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/prerak/IIIT/cg/ass1/2DGame/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
